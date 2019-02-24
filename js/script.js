64172405417ee75c6ffc239af4a36ee761a4e72b
LizaDemyanovska <hiscaramel@gmail.com>
2019-02-24 19:08:01 +0200

    add ajax request

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
 $(document).ready(function() {
     $("#phoneNumber").mask("+38 0(99) 999-99-99");
+
+    $("#contact-form-submit").click(function(){
+    	var data = new FormData();
+    	data.append('email', $('#emailInput').val());
+    	data.append('file', $('#fileInput')[0].files[0]);
+    	data.append('tel', $('#phoneNumber').val());
+
+    	$.ajax({
+    		contentType: false,
+      		processData: false,
+      		data: data,
+      		url: "/controller/action.php",
+      		success: function(response){
+      			console.log(response);
+      		}
+    	});
+    });
   });
+
